5ec5b3ffdf3a9da1010fd1c0497bab46f602e56b
Dzhavat Ushev <dzhavatushev@gmail.com>
2020-01-15 22:15:22 +0100

    Add commands

--- src/webviewContent.ts
+++ src/webviewContent.ts
@@ -22,7 +22,10 @@ export function getWebviewContent(styleSrc: Uri) {
 			<ul>
 				<li><a href="#local-changes">Local Changes</a></li>
 				<li><a href="#branches">Branches</a></li>
-				<li><a href="#commit-history">Commit History</a></li>
+				<li><a href="#remote-repo">Working with a Remote Repository</a></li>
+				<li><a href="#see-commit-history">See Commit History</a></li>
+				<li><a href="#rebase">Rebase</a></li>
+				<li><a href="#undo">Undo</a></li>
 				<li><a href="#stash">Stash</a></li>
 				<li><a href="#repo-setup">Repository Setup</a></li>
 				<li><a href="#global-config">Global Config</a></li>
@@ -34,13 +37,16 @@ export function getWebviewContent(styleSrc: Uri) {
 		<p>Display the status of modified files</p>
 		<pre>git status</pre>
 
-		<p>Add a file as it looks now to staging</p>
+		<p>Add a file to staging as it looks right now</p>
 		<pre>git add [file]</pre>
 
-		<p>Commit your staged file as a new commit</p>
+		<p>Add a folder to staging as it looks right now</p>
+		<pre>git add [folder]</pre>
+
+		<p>Commit staged files in a new commit</p>
 		<pre>git commit -m "descriptive_message"</pre>
 
-		<p>Add all files to staging and commit them</p>
+		<p>Add all files to staging and commit them at once</p>
 		<pre>git commit -am "descriptive_message"</pre>
 
 		<p>Unstage a file while retaining the changes</p>
@@ -49,9 +55,12 @@ export function getWebviewContent(styleSrc: Uri) {
 		<p>Diff of what is changed but not staged</p>
 		<pre>git diff</pre>
 
+		<p>Diff of what has changed between staged changes and the last commit</p>
+		<pre>git diff --staged</pre>
+
 		<h2 id="branches">Branches</h2>
 
-		<p>List all branches</p>
+		<p>List all branches. The current one is marked with <strong>*</strong></p>
 		<pre>git branch</pre>
 
 		<p>Create a new branch</p>
@@ -69,10 +78,33 @@ export function getWebviewContent(styleSrc: Uri) {
 		<p>Delete a branch, locally</p>
 		<pre>git branch -d [branch_name]</pre>
 
-		<p>Merge a branch in the current branch</p>
+		<p>Merge another branch into the current one</p>
 		<pre>git merge [branch_name]</pre>
 		
-		<h2 id="commit-history">Commit History</h2>
+		<h2 id="remote-repo">Working with a Remote Repository</h2>
+
+		<p>Fetch and merge all commits from the tracked remote branch</p>
+		<pre>git pull</pre>
+
+		<p>Fetch and merge all commits from a specific remote branch</p>
+		<pre>git pull [alias] [branch_name]</pre>
+
+		<p>Fetch recent changes from the tracked remote branch but don't merge them</p>
+		<pre>git fetch</pre>
+
+		<p>Push all local branch commits to the tracked remote branch</p>
+		<pre>git push</pre>
+
+		<p>Push all local branch commits to a specific remote branch</p>
+		<pre>git push [alias] [branch_name]</pre>
+
+		<p>Add a new remote repository with the given alias</p>
+		<pre>git remote add [alias] [repo_url]</pre>
+
+		<p>Display a list of remote repositories and their URLs</p>
+		<pre>git remote -v</pre>
+
+		<h2 id="see-commit-history">See Commit History</h2>
 		
 		<p>Show all commits in the current branch’s history</p>
 		<pre>git log</pre>
@@ -80,6 +112,28 @@ export function getWebviewContent(styleSrc: Uri) {
 		<p>Show all commits in the current branch’s history by printing each commit on a single line</p>
 		<pre>git log --oneline</pre>
 		
+		<h2 id="rebase">Rebase</h2>
+
+		<p>Reapply commits from the current branch on top of another base</p>
+		<pre>git rebase [branch]</pre>
+
+		<p>Abort a rebase</p>
+		<pre>git rebase –-abort</pre>
+
+		<p>Continue a rebase after resolving conflicts</p>
+		<pre>git rebase –-continue</pre>
+
+		<h2 id="undo">Undo</h2>
+
+		<p>Revert the changes in a commit and record them in a new commit</p>
+		<pre>git revert [commit]</pre>
+
+		<p>Reset to a previous commit and preserve the changes made since [commit] as unstaged</p>
+		<pre>git reset [commit]</pre>
+
+		<p>Reset to a previous commit and discard the changes made since the [commit]</p>
+		<pre>git reset --hard [commit]</pre>
+
 		<h2 id="stash">Stash</h2>
 
 		<p>Stash modified and staged changes</p>
@@ -102,6 +156,9 @@ export function getWebviewContent(styleSrc: Uri) {
 		<p>Create an empty repository in the current folder</p>
 		<pre>git init</pre>
 
+		<p>Create an empty repository in a specific folder</p>
+		<pre>git init [folder]</pre>
+		
 		<p>Clone a repository and add it to the current folder</p>
 		<pre>git clone [repo_url]</pre>
 
