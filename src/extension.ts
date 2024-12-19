79e1c3cc10432757302da027d52424a6abdb9800
Dzhavat Ushev <dzhavatushev@gmail.com>
2024-12-19 22:45:21 +0100

    chore: format file

--- src/extension.ts
+++ src/extension.ts
@@ -1,41 +1,49 @@
-import * as vscode from 'vscode';
-import { join } from 'path';
+import * as vscode from "vscode";
+import { join } from "path";
 
-import { getWebviewContent } from './webviewContent';
+import { getWebviewContent } from "./webviewContent";
 
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('open.git.cheatsheet', () => {
-		const assetsRoot = vscode.Uri.file(join(context.extensionPath, 'assets'));
+  let disposable = vscode.commands.registerCommand(
+    "open.git.cheatsheet",
+    () => {
+      const assetsRoot = vscode.Uri.file(join(context.extensionPath, "assets"));
 
-		const panel = vscode.window.createWebviewPanel(
-			'gitCheatsheet',
-			'Git Cheatsheet',
-			vscode.ViewColumn.Beside, {
-				localResourceRoots: [assetsRoot],
-				enableScripts: true
-			}
-		);
+      const panel = vscode.window.createWebviewPanel(
+        "gitCheatsheet",
+        "Git Cheatsheet",
+        vscode.ViewColumn.Beside,
+        {
+          localResourceRoots: [assetsRoot],
+          enableScripts: true,
+        }
+      );
 
-		const assetsPath = panel.webview.asWebviewUri(assetsRoot);
-		const cspSource = panel.webview.cspSource;
-		
-		panel.webview.html = getWebviewContent(cspSource, assetsPath);
+      const assetsPath = panel.webview.asWebviewUri(assetsRoot);
+      const cspSource = panel.webview.cspSource;
 
-		panel.webview.onDidReceiveMessage(
-			(command: string) => {
-				vscode.env.clipboard.writeText(command)
-					.then(() => {
-						vscode.window.showInformationMessage(`Command copied: ${command}`);
-					}, () => {
-						vscode.window.showErrorMessage('Copy failed');
-					});
-			},
-			undefined,
-			context.subscriptions
-		);
-	});
+      panel.webview.html = getWebviewContent(cspSource, assetsPath);
 
-	context.subscriptions.push(disposable);
+      panel.webview.onDidReceiveMessage(
+        (command: string) => {
+          vscode.env.clipboard.writeText(command).then(
+            () => {
+              vscode.window.showInformationMessage(
+                `Command copied: ${command}`
+              );
+            },
+            () => {
+              vscode.window.showErrorMessage("Copy failed");
+            }
+          );
+        },
+        undefined,
+        context.subscriptions
+      );
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
